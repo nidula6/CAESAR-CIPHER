7c933279a2603a9c831b4f3ecec2cbc21cf4513a
kavinda2024 <kavindanelum.kill@gmail.com>
2025-05-31 00:14:11 +0530

    implemented encryption and decryption functions in main file

--- main.c
+++ main.c
@@ -14,5 +14,38 @@ void processMessage(char message[], char result[], int shiftKey, struct Circular
 
 int main() {
 
+struct CircularQueue alphabetQueue;
+    initializeQueue(&alphabetQueue);
+
+    char choice;
+    char inputMessage[1200];
+    char outputMessage[1200];
+    int shiftKey;
+
+    printf("Encryption (e) or Decryption (d)?: "); //prompt the user if they want to encrypt or decrypt
+    scanf(" %c", &choice);
+    getchar();
+
+    printf("Enter your message: "); //prompt the user to enter the msg
+    fgets(inputMessage, sizeof(inputMessage), stdin);
+    inputMessage[strcspn(inputMessage, "\n")] = '\0';
+
+    printf("Enter the shift key: "); //prompt the user enter how many letters to move forward or backward
+    scanf("%d", &shiftKey);
+
+    if (choice == 'd') {
+        shiftKey = -shiftKey;
+    }
+
+    processMessage(inputMessage, outputMessage, shiftKey, &alphabetQueue);
+
+    printf("*****************************\n");
+    if (choice == 'e') {
+        printf("Encrypted message: %s\n", outputMessage);  //display the encrypted msg
+    } else {
+        printf("Original message: %s\n", outputMessage);   //display the original msg
+    }
+
+
     return 0;
 }
